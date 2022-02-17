0c25c40a2761aeb2fbecde8b06d93cb3f7b99438
Mahdi Shirmohammadi <66047572+PrinceMahdi@users.noreply.github.com>
2022-02-16 22:11:44 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import hashlib
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(f'Error Fetching {response.status_code}, Try again.')
+    return response
+
+
+def get_pass_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_pass_leaks_count(response, tail)
+
+
+def main(password):
+    count = pwned_api_check(password)
+    if count:
+        print(f'\'{password}\' was found {count} times. You should probably change your password.')
+    else:
+        print(f'{password} was NOT found. It\'t safe to keep using it.')
+
+
+main('Password123')
